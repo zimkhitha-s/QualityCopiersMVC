1663c542879c4678edf7f280b03f98c448a14189
nyanem <ST10026525@vcconnect.edu.za>
2025-08-21 10:12:32 +0200

    Created Navigation bar in layout file. Created Dashboard Controller

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace INSY7315_ElevateDigitalStudios_POE.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
