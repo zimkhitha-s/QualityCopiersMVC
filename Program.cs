a591f193c49f4dd21a8368b24846b621430447ee
Thando Fredericks <st10187895@vcconnect.edu.za>
2025-10-28 16:57:22 +0200

    Notifications Controller + View

--- Program.cs
+++ Program.cs
@@ -72,7 +72,7 @@ namespace INSY7315_ElevateDigitalStudios_POE
             // Map your routes
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
             app.Run();
         }
